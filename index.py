0666300f47830d667f225175827cc20bd1c81e35
Amir <testnet.binance.futuers@gmail.com>
2025-10-29 11:09:52 +0330

    initial  commit

---
+++ index.py
@@ -0,0 +1,9 @@
+year=int(input("Please Enter your birthdate:"))
+current_year=2025
+age=current_year-year
+hoursOfLife=age*365*24
+minutesOfLife=age*365*24*60
+secondsOfLife=age*365*24*60*60
+print(f"Your age to hour:{hoursOfLife}")
+print(f"Your age to minute:{minutesOfLife}")
+print(f"Your age to hour:{secondsOfLife}")
